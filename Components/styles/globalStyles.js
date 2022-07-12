5eb9411532cf20ccf8390f3f75c7f39b90773e3c
idemmanuel001 <idemmanuel001@gmail.com>
2022-07-12 23:42:34 +0100

    fixed netlify built name sensitivity issues

---
+++ Components/styles/globalStyles.js
@@ -0,0 +1,103 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+ 
+    *{
+    padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
+    } 
+
+    *::after,
+    *::before,
+    *::content{
+    padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
+    }
+
+    :root{
+        font-size: 1rem;
+        scroll-behavior: smooth;
+    }
+
+    html{
+    width: 100%;
+    height: 100vh;
+    }
+
+    body{
+    width: 100%;
+    height: 100%;
+    font-family:  -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    color: ${({theme})=> theme.black};
+    background-color: white;
+    }
+
+    #__next{
+    width: 100%;
+    height: 100%;
+    }
+
+    ul {
+    list-style-type: none;
+    }
+
+    a {
+    text-decoration: none;
+    }
+
+    h1{
+        font-size: ${({theme}) => theme.largeX2};
+
+        @media(min-width: ${({ theme }) => theme.tablet}){
+            font-size: ${({theme}) => theme.largeX3};
+            margin: 0.1 0;
+        }
+
+     /*    @media(min-width: ${({ theme }) => theme.desktop}){
+            font-size: ${({theme}) => theme.largeX4};
+            margin: 0.3 0;
+         } */
+    }
+
+    h1,
+    h2{
+    font-weight: 700;
+    }
+
+    h2{
+     font-size: ${(theme) => theme.large};
+
+     @media(${(theme)=> theme.desktop}){
+         font-size: ${(theme) => theme.large};
+         margin: 0.2 0;
+     }
+    }
+
+    h3{
+        font-weight: 600;
+        font-size: ${(theme) => theme.medium};
+
+        @media(${(theme) => theme.desktop}){
+            font-size: ${(theme) => theme.large};
+            margin: 0.1 0;
+        }
+    }
+
+    p{
+        font-weight: 400;
+        font-size: ${(theme) => theme.medium};
+
+        @media(${(theme) => theme.tablet}){
+            font-size: ${(theme) => theme.large};
+        }
+    }
+
+    img{
+        width: 100%;
+    }
+
+`;
+
+export default GlobalStyles;
---
+++ Components/styles/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    primaryColor: '#BBCBC1',
+    secondaryColor: '#077333',
+    accentColor: '#0BCE5A',
+    black: '#060809',
+    gray: '#C9CFD4',
+    white: '#ffffff',
+    verySmall: '0.875rem',
+    small: '1rem',
+    medium: '1.3rem',
+    large: '1.8rem',
+    largeX2: '2.2rem',
+    largeX3: '3rem',
+    mobile: '320px',
+    tablet: '768px',
+    desktop: '990px',
+    maxWidth: '1200px'
+};
+
