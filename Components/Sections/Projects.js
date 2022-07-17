aba2892b16c1c4f47ab5e41cbb046039cbf0daf1
idemmanuel001 <idemmanuel001@gmail.com>
2022-07-17 18:12:28 +0100

    refactored the project section

--- Components/Sections/Projects.js
+++ Components/Sections/Projects.js
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
 import { Container, Section } from '../styles/sharedStyles';
 
 
-//STYLE
+//Project Style
 const StyledProject = styled(Section)`
     color:  ${({ theme }) => theme.primaryColor};
     background:  ${({ theme }) => theme.black};
@@ -23,74 +24,13 @@ const StyledProject = styled(Section)`
            margin: 2rem auto;
 
         }
-
-        .projectItem{
-            width: 320px;
-            height: fit-content;
-            margin: 1rem auto;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            border-radius: 20px;
-            overflow: hidden;
-            background: ${({ theme }) => theme.primaryColor};
-          
-            color: ${({ theme }) => theme.black};
-
-            @media(min-width: ${({ theme }) => theme.tablet}){
-                margin: 2rem 0;
-            }
-        }
-
-        .projectBody{
-            padding: 0.5rem 1rem 1rem 1rem;
-            margin: 0 auto;
-
-            h3{
-                font-weight: bold;
-                font-size: ${({ theme }) => theme.large};
-            }
-
-            p{
-                font-weight: 400;
-            }
-
-            .stack{
-                margin: 0.5rem 0;
-
-                span{
-                    font-weight: 600;
-                    text-decoration: underline;
-                }
-            }
-
-            .view{
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
-                margin: 0.8rem 0;
-            }
-            a{
-                background: ${({ theme }) => theme.black};
-                color: ${({ theme }) => theme.white};
-                padding: 0.3rem 0.8rem;
-                margin-right: 0.8rem;
-                border-radius: 8px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-
-
-            }
-        }
-
-
     }
 
 `;
 
-const Projects = ({ projects }) => {
 
+//Project Markup
+const Projects = ({ projects }) => {
 
     return (
         <StyledProject id='projects'>
@@ -105,36 +45,8 @@ const Projects = ({ projects }) => {
                     <div className="projectsContainer">
                         {projects.map((project, index) => (
 
-                            <div
-                                key={index}
-                                className="projectItem">
+                            <ProjectItem key={index} project={project} />
 
-                                <div className='projectImage'>
-                                    <Image
-                                        src={project.frontmatter.image}
-                                        alt={project.frontmatter.title}
-                                        width='320'
-                                        height='180'
-                                        objectFit='fill' />
-                                </div>
-
-                                <div className="projectBody">
-
-                                    <h3> {project.frontmatter.title} </h3>
-
-                                    <p className='stack'>
-                                        <span>Stack: </span>
-                                        {project.frontmatter.stack.join(',')}
-                                    </p>
-
-                                    <p className='description'> {project.frontmatter.description} </p>
-
-                                    <div className="view">
-                                        <a href={project.frontmatter.live} className="href"> live site</a>
-                                        <a href={project.frontmatter.code} className="href"> view code</a>
-                                    </div>
-                                </div>
-                            </div>
 
                         ))}
                     </div>
@@ -144,5 +56,125 @@ const Projects = ({ projects }) => {
         </StyledProject>
     );
 };
+
+
+
+//ProjectItem styles
+const StyledProJectItem = styled.div`
+    width: 320px;
+    height: fit-content;
+    margin: 1rem auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 2px -2px 5px  ${({ theme }) => theme.darkGray};
+    background: ${({ theme }) => theme.primaryColor};
+    
+    color: ${({ theme }) => theme.black};
+
+    @media(min-width: ${({ theme }) => theme.tablet}){
+        margin: 2rem 0;
+    }
+
+    .projectBody{
+        padding: 0.5rem 1rem 1rem 1rem;
+        margin: 0 auto;
+
+        h3{
+            font-weight: bold;
+            font-size: ${({ theme }) => theme.large};
+        }
+
+        p{
+            font-weight: 400;
+        }
+
+        .stack{
+            margin: 0.5rem 0;
+
+            span{
+                font-weight: 600;
+                text-decoration: underline;
+            }
+        }
+
+        .description{
+
+            span{
+                margin-left: 0.5rem;
+                cursor: pointer;
+                text-decoration: underline;
+            }
+        }
+
+        .view{
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            margin: 0.8rem 0;
+        }
+        a{
+            background: ${({ theme }) => theme.black};
+            color: ${({ theme }) => theme.white};
+            padding: 0.3rem 0.8rem;
+            margin-right: 0.8rem;
+            border-radius: 8px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+
+        }
+    }
+
+
+`;
+
+//ProjectItem Markup
+function ProjectItem({ project }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const { frontmatter: { title, description, image, stack, code, live }, content } = project;
+    return (
+        <StyledProJectItem>
+
+            <div className='projectImage'>
+                <Image
+                    src={image}
+                    alt={title}
+                    width='320'
+                    height='180'
+                    objectFit='fill' />
+            </div>
+
+            <div className="projectBody">
+
+                <h3> {title} </h3>
+
+                <p className='stack'>
+                    <span>Stack: </span>
+                    {stack.join(', ')}
+                </p>
+
+                <p className='description'> {isOpen ? content : description + '...'}
+                    <span onClick={() => setIsOpen(!isOpen)} >
+                        {isOpen ? 'view less' : 'view more'}
+                    </span>
+                </p>
+
+                <div className="view">
+                    <a href={live} className="href"> live site</a>
+                    <a href={code} className="href"> view code</a>
+                </div>
+            </div>
+
+
+        </StyledProJectItem>
+    );
+}
+
 
 export default Projects;
